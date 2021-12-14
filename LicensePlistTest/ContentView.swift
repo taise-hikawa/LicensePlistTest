570f6fc482902c27ab2df445e83984d30f096273
taise-hikawa <cmmkarubi@gmail.com>
2021-12-14 18:15:48 +0900

    Initial Commit

---
+++ LicensePlistTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LicensePlistTest
+//
+//  Created by 樋川大聖 on 2021/12/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LicensePlistTest/LicensePlistTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LicensePlistTestApp.swift
+//  LicensePlistTest
+//
+//  Created by 樋川大聖 on 2021/12/14.
+//
+
+import SwiftUI
+
+@main
+struct LicensePlistTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
